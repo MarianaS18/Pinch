9db82fec140a5fcfe07708216135236d510dc3dd
Mariana <mariana@fastmail.net>
2022-05-27 11:17:25 +0200

    setting up the project

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Mariana Steblii on 27/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
